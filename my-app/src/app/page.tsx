1040121631cdb118ed2a70c8ac2cf187d63e2fc8
MarcioTezini <125216531+MarcioTezini@users.noreply.github.com>
2025-01-23 21:24:16 -0300

    ajuste

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,45 +1,12 @@
 "use client";
-import { useEffect, useState } from "react";
-
 export default function Home() {
-  type Item = {
-    id: number,
-    name: string,
-    phone: number
-  }
-
-  const [data, setData] = useState<Item[] | null>(null)
-  useEffect(() => {
-    
-    async function loadData() {
-      const data = await fetch('http://localhost:3022/')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setData(data)
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error('Erro ao buscar dados:', error);
-      });
-      console.log('aquiiiii')
-      console.log(data, 'data')
-    }
-    loadData()
-  }, [])
 
   return (
     <div className="bg-white text-center">
-      {data?.map((item) => (
-        <div key={item.id}>
-          <h1 className="text-neutral-950">{item.name}</h1>
-          <p className="text-neutral-950">{item.phone}</p>
+        <div>
+          <h1 className="text-slate-100">tenso</h1>
+          <p className="text-slate-100">hoje</p>
         </div>
-      ))}
     </div>
   );
 }
